c71ac7a7fb64d574dafe7e38425bb166b5be8b23
David Taillois <david.taillois@protonmail.com>
2022-01-24 22:32:44 +0100

    stop traefik at the end

--- docker_stop.sh
+++ docker_stop.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker-compose  -f ./traefik/docker-compose.yaml down
 docker-compose  -f ./nextcloud/docker-compose.yaml down
+docker-compose  -f ./traefik/docker-compose.yaml down

220518e7966a8e3361331ab1d0760d0a11c49d87
David Taillois <david.taillois@protonmail.com>
2022-01-25 01:24:28 +0100

    stable version

--- docker_start.sh
+++ docker_start.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker pull collabora/code:latest
-docker pull nextcloud:latest
-docker pull mariadb:latest
-docker pull traefik:latest
+#docker pull collabora/code:latest
+#docker pull nextcloud:latest
+#docker pull mariadb:latest
+#docker pull traefik:latest
 
 docker-compose -f traefik/docker-compose.yaml up -d --build
 docker-compose -f nextcloud/docker-compose.yaml up -d --build
